1434c4faf3db4855d16a6a762d19117db3778ba1
Giovana Assis <giovana.sant@hotmail.com>
2022-09-04 16:30:25 -0300

    First component - ServerList

---
+++ src/Components/ServerList/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <ServerButton isHome />
+    <div class="separator"></div>
+    <ServerButton :mentions="3" />;
+    <ServerButton hasNotification />
+    <ServerButton :mentions="32" hasNotification />
+    <ServerButton hasNotification />
+    <ServerButton hasNotification />
+    <ServerButton hasNotification />
+    <ServerButton :mentions="4" />
+    <ServerButton v-for="servers in 18" :key="servers.id" />
+  </div>
+</template>
+
+<script>
+import ServerButton from "./ServerButton.vue";
+
+export default {
+  components: {
+    ServerButton
+  }
+};
+</script>
+
+<style scoped lang>
+.container {
+  grid-area: SL;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--tertiary);
+  padding: 11px 0;
+  max-height: 100vh;
+  overflow-y: scroll;
+}
+.container::-webkit-scrollbar {
+  display: none;
+}
+
+.separator {
+  width: 32px;
+  border-bottom: solid 2px var(--quaternary);
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+import Layout from "./Components/Layout";
+import "./styles/main.css";
+
+new Vue({
+  el: '#app',
+  render: h => h(Layout)
+});
